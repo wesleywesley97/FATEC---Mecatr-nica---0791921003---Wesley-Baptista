cbcc381a7b8b297ab3bef28c799cc5e58dfdb888
wesleywesley97 <70909816+wesleywesley97@users.noreply.github.com>
2020-09-08 08:57:19 -0300

    Create programa03.py

---
+++ Prática03/programa03.py
@@ -0,0 +1,5 @@
+#Pede ao usuário dois números e realiza sua soma
+numero1 = int(input('informe uma valor:'))
+numero2 = int(input('informe outro valor:'))
+resultado = numero1 + numero2
+print('resultado da soma:', resultado)
