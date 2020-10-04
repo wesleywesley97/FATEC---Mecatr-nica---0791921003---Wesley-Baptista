9eed336103b853006682db92c5267b579e0b6fcc
wesleywesley97 <70909816+wesleywesley97@users.noreply.github.com>
2020-10-04 13:02:03 -0300

    Create programa05.py

---
+++ Prática03/programa05.py
@@ -0,0 +1,6 @@
+#Volume do cilindro
+raio_do_cilindro = float(input('informe o r do cilindro:'))
+área_da_base = 3.14 * raio_do_cilindro ** 2
+altura_do_cilindro = float(input('informe a h:'))
+volume_do_cilindro = área_da_base * altura_do_cilindro
+print('valor do volume do cilindro', volume_do_cilindro)
